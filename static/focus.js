80291fcc4fc414510f88218b946eaa0c590a6398
Vladik74 <vlad06012002@mail.ru>
2022-04-25 18:23:32 +0500

    Vladik Bikbulatov && Nastya Melnikova FT-203

--- static/focus.js
+++ static/focus.js
@@ -5,37 +5,29 @@ const API = {
     buhForms: "/api3/buh",
 };
 
-function run() {
-    sendRequest(API.organizationList, (orgOgrns) => {
-        const ogrns = orgOgrns.join(",");
-        sendRequest(`${API.orgReqs}?ogrn=${ogrns}`, (requisites) => {
-            const orgsMap = reqsToMap(requisites);
-            sendRequest(`${API.analytics}?ogrn=${ogrns}`, (analytics) => {
-                addInOrgsMap(orgsMap, analytics, "analytics");
-                sendRequest(`${API.buhForms}?ogrn=${ogrns}`, (buh) => {
-                    addInOrgsMap(orgsMap, buh, "buhForms");
-                    render(orgsMap, orgOgrns);
-                });
-            });
-        });
-    });
+async function run() {
+    const orgOgrns = await sendRequest(API.organizationList);
+    const ogrns = orgOgrns.join(",");
+    const response1 = sendRequest(`${API.orgReqs}?ogrn=${ogrns}`);
+    const response2 = sendRequest(`${API.analytics}?ogrn=${ogrns}`);
+    const response3 = sendRequest(`${API.buhForms}?ogrn=${ogrns}`);
+    const [requisites, analytics, buh] = await Promise.all([response1, response2, response3]);
+    const orgsMap = reqsToMap(requisites);
+    addInOrgsMap(orgsMap, analytics, "analytics");
+    addInOrgsMap(orgsMap, buh, "buhForms");
+    render(orgsMap, orgOgrns);
 }
 
 run();
 
-function sendRequest(url, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                callback(JSON.parse(xhr.response));
-            }
+function sendRequest(url) {
+    return fetch(url).then((response) => {
+        if (response.ok) {
+            return response.json();
         }
-    };
-
-    xhr.send();
+        alert(`${response.status}: ${response.statusText}`);
+        throw new Error(`${response.status}: ${response.statusText}`);
+    });
 }
 
 function reqsToMap(requisites) {
@@ -86,7 +78,7 @@ function renderOrganization(orgInfo, template, container) {
                 orgInfo.buhForms[orgInfo.buhForms.length - 1].form2[0] &&
                 orgInfo.buhForms[orgInfo.buhForms.length - 1].form2[0]
                     .endValue) ||
-                0
+            0
         );
     } else {
         money.textContent = "—";

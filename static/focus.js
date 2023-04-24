6aeae9d6e70ac5a2a56abc0270de493032dfe421
Danil_Titarenko <danil.titarenko@gmail.com>
2023-04-24 14:08:47 +0500

    1-4 done

--- static/focus.js
+++ static/focus.js
@@ -5,37 +5,56 @@ const API = {
     buhForms: "/api3/buh",
 };
 
-function run() {
-    sendRequest(API.organizationList, (orgOgrns) => {
-        const ogrns = orgOgrns.join(",");
-        sendRequest(`${API.orgReqs}?ogrn=${ogrns}`, (requisites) => {
-            const orgsMap = reqsToMap(requisites);
-            sendRequest(`${API.analytics}?ogrn=${ogrns}`, (analytics) => {
-                addInOrgsMap(orgsMap, analytics, "analytics");
-                sendRequest(`${API.buhForms}?ogrn=${ogrns}`, (buh) => {
-                    addInOrgsMap(orgsMap, buh, "buhForms");
-                    render(orgsMap, orgOgrns);
-                });
-            });
-        });
-    });
+async function run() {
+    // try {
+    //     const orgOgrns = await sendRequest(API.organizationList);
+    //     const ogrns = orgOgrns.join(",");
+    //     const [requisites, analytics, buh] = await Promise.all([sendRequest(`${API.orgReqs}?ogrn=${ogrns}`),
+    //         sendRequest(`${API.analytics}?ogrn=${ogrns}`),
+    //         sendRequest(`${API.buhForms}?ogrn=${ogrns}`)])
+    //     const orgsMap = reqsToMap(requisites);
+    //     addInOrgsMap(orgsMap, analytics, "analytics");
+    //     addInOrgsMap(orgsMap, buh, "buhForms");
+    //     render(orgsMap, orgOgrns);
+    // } catch (e) {
+    //     alert(e.message);
+    // }
+    let orgOgrns = await sendRequest(API.organizationList);
+    const ogrns = orgOgrns.join(",");
+    let requisites = await sendRequest(`${API.orgReqs}?ogrn=${ogrns}`);
+    let analytics = await sendRequest(`${API.analytics}?ogrn=${ogrns}`);
+    let buh = await sendRequest(`${API.buhForms}?ogrn=${ogrns}`);
+    const promAll = await Promise.all([requisites, analytics, buh])
+    const orgsMap = reqsToMap(promAll[0]);
+    addInOrgsMap(orgsMap, analytics, "analytics");
+    addInOrgsMap(orgsMap, buh, "buhForms");
+    render(orgsMap, orgOgrns);
 }
 
 run();
 
-function sendRequest(url, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                callback(JSON.parse(xhr.response));
-            }
-        }
-    };
-
-    xhr.send();
+async function sendRequest(url) {
+    // return new Promise((resolve, reject) => {
+    //         const xhr = new XMLHttpRequest();
+    //         xhr.open("GET", url, true);
+    //
+    //         xhr.onreadystatechange = function () {
+    //             if (xhr.readyState === XMLHttpRequest.DONE) {
+    //                 if (xhr.status === 200) {
+    //                     resolve(JSON.parse(xhr.response));
+    //                 }
+    //             }
+    //         };
+    //         xhr.send();
+    //     }
+    // )
+    const response = await fetch(url);
+    if (response.ok) {
+        return response.json();
+    } else {
+        alert(`Ошибка № ${response.status}`);
+        return Promise.reject(response.status);
+    }
 }
 
 function reqsToMap(requisites) {
@@ -86,7 +105,7 @@ function renderOrganization(orgInfo, template, container) {
                 orgInfo.buhForms[orgInfo.buhForms.length - 1].form2[0] &&
                 orgInfo.buhForms[orgInfo.buhForms.length - 1].form2[0]
                     .endValue) ||
-                0
+            0
         );
     } else {
         money.textContent = "—";

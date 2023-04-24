5353ba9852af24176c52e21eb7da8f5e8fb32371
Sokolova Anna <anna.sok.03@mail.ru>
2023-04-24 17:25:39 +0500

    done

--- static/focus.js
+++ static/focus.js
@@ -1,15 +1,16 @@
-const API = {
+let API = {
     organizationList: "/orgsList",
     analytics: "/api3/analytics",
     orgReqs: "/api3/reqBase",
     buhForms: "/api3/buh",
 };
 
+/*
 function run() {
     sendRequest(API.organizationList, (orgOgrns) => {
-        const ogrns = orgOgrns.join(",");
+        let ogrns = orgOgrns.join(",");
         sendRequest(`${API.orgReqs}?ogrn=${ogrns}`, (requisites) => {
-            const orgsMap = reqsToMap(requisites);
+            let orgsMap = reqsToMap(requisites);
             sendRequest(`${API.analytics}?ogrn=${ogrns}`, (analytics) => {
                 addInOrgsMap(orgsMap, analytics, "analytics");
                 sendRequest(`${API.buhForms}?ogrn=${ogrns}`, (buh) => {
@@ -20,11 +21,30 @@ function run() {
         });
     });
 }
+*/
+
+async function run() {
+    let orgOgrns = await sendRequest(API.organizationList);
+    let ogrns = orgOgrns.join(",");
+
+    let [requisites, analytics, buh] = await Promise.all([
+        sendRequest(`${API.orgReqs}?ogrn=${ogrns}`),
+        sendRequest(`${API.analytics}?ogrn=${ogrns}`),
+        sendRequest(`${API.buhForms}?ogrn=${ogrns}`)
+    ]);
+
+    let orgsMap = reqsToMap(requisites);
+
+    addInOrgsMap(orgsMap, analytics, "analytics");
+    addInOrgsMap(orgsMap, buh, "buhForms");
+    render(orgsMap, orgOgrns);
+}
 
 run();
 
+/*
 function sendRequest(url, callback) {
-    const xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
 
     xhr.onreadystatechange = function () {
@@ -37,6 +57,37 @@ function sendRequest(url, callback) {
 
     xhr.send();
 }
+*/
+/*
+function sendRequest(url) { // xhr
+    return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+            xhr.open("GET", url, true);
+
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === XMLHttpRequest.DONE)
+                    if (xhr.status === 200)
+                        resolve(JSON.parse(xhr.response));
+                    else
+                        reject(xhr.status);
+            };
+
+            xhr.send();
+        }
+    )
+}
+*/
+
+async function sendRequest(url) { // fetch
+    const response = await fetch(url);
+    if (response.ok)
+        return response.json();
+    // else
+    //     throw new Error(response.status);
+
+    //console.log(`${response.status} ${response.statusText}`);
+    alert(`${response.status} - ${response.statusText}`);
+}
 
 function reqsToMap(requisites) {
     return requisites.reduce((acc, item) => {
@@ -46,17 +97,17 @@ function reqsToMap(requisites) {
 }
 
 function addInOrgsMap(orgsMap, additionalInfo, key) {
-    for (const item of additionalInfo) {
+    for (let item of additionalInfo) {
         orgsMap[item.ogrn][key] = item[key];
     }
 }
 
 function render(organizationsInfo, organizationsOrder) {
-    const table = document.getElementById("organizations");
+    let table = document.getElementById("organizations");
     table.classList.remove("hide");
 
-    const template = document.getElementById("orgTemplate");
-    const container = table.querySelector("tbody");
+    let template = document.getElementById("orgTemplate");
+    let container = table.querySelector("tbody");
 
     organizationsOrder.forEach((item) => {
         renderOrganization(organizationsInfo[item], template, container);
@@ -64,11 +115,11 @@ function render(organizationsInfo, organizationsOrder) {
 }
 
 function renderOrganization(orgInfo, template, container) {
-    const clone = document.importNode(template.content, true);
-    const name = clone.querySelector(".name");
-    const indebtedness = clone.querySelector(".indebtedness");
-    const money = clone.querySelector(".money");
-    const address = clone.querySelector(".address");
+    let clone = document.importNode(template.content, true);
+    let name = clone.querySelector(".name");
+    let indebtedness = clone.querySelector(".indebtedness");
+    let money = clone.querySelector(".money");
+    let address = clone.querySelector(".address");
 
     name.textContent =
         (orgInfo.UL && orgInfo.UL.legalName && orgInfo.UL.legalName.short) ||
@@ -92,7 +143,7 @@ function renderOrganization(orgInfo, template, container) {
         money.textContent = "—";
     }
 
-    const addressFromServer = orgInfo.UL.legalAddress.parsedAddressRF;
+    let addressFromServer = orgInfo.UL.legalAddress.parsedAddressRF;
     address.textContent = createAddress(addressFromServer);
 
     container.appendChild(clone);
@@ -102,8 +153,8 @@ function formatMoney(money) {
     let formatted = money.toFixed(2);
     formatted = formatted.replace(".", ",");
 
-    const rounded = money.toFixed(0);
-    const numLen = rounded.length;
+    let rounded = money.toFixed(0);
+    let numLen = rounded.length;
     for (let i = numLen - 3; i > 0; i -= 3) {
         formatted = `${formatted.slice(0, i)} ${formatted.slice(i)}`;
     }
@@ -112,7 +163,7 @@ function formatMoney(money) {
 }
 
 function createAddress(address) {
-    const addressToRender = [];
+    let addressToRender = [];
     if (address.regionName) {
         addressToRender.push(createAddressItem("regionName"));
     }

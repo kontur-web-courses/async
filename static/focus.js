004411586c739a2abd26d4ee9f256bb750438876
Timur Rakhimianov <raktagir2@gmail.com>
2023-04-24 14:05:02 +0500

    1

--- static/focus.js
+++ static/focus.js
@@ -5,37 +5,30 @@ const API = {
     buhForms: "/api3/buh",
 };
 
-function run() {
-    sendRequest(API.organizationList, (orgOgrns) => {
-        const ogrns = orgOgrns.join(",");
-        sendRequest(`${API.orgReqs}?ogrn=${ogrns}`, (requisites) => {
-            const orgsMap = reqsToMap(requisites);
-            sendRequest(`${API.analytics}?ogrn=${ogrns}`, (analytics) => {
-                addInOrgsMap(orgsMap, analytics, "analytics");
-                sendRequest(`${API.buhForms}?ogrn=${ogrns}`, (buh) => {
-                    addInOrgsMap(orgsMap, buh, "buhForms");
-                    render(orgsMap, orgOgrns);
-                });
-            });
-        });
-    });
+async function run() {
+    const orgOgrns = await sendRequest(API.organizationList);
+    const ogrns = orgOgrns.join(",");
+    const response1 = sendRequest(`${API.orgReqs}?ogrn=${ogrns}`);
+    const response2 = sendRequest(`${API.analytics}?ogrn=${ogrns}`);
+    const response3 = sendRequest(`${API.buhForms}?ogrn=${ogrns}`);
+    const [requisites, analytics, buh] = await Promise.all([response1, response2, response3]);
+    const orgsMap = reqsToMap(requisites);
+    addInOrgsMap(orgsMap, analytics, "analytics");
+    addInOrgsMap(orgsMap, buh, "buhForms");
+    render(orgsMap, orgOgrns);
 }
-
 run();
 
-function sendRequest(url, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                callback(JSON.parse(xhr.response));
+async function sendRequest(url) {
+    return new Promise((resolve, reject) => {
+        fetch(url).then(response => {
+            if (response.status >= 300) {
+                alert(response.status + " " + response.statusText);
+            } else {
+                resolve(response.json());
             }
-        }
-    };
-
-    xhr.send();
+        });
+    });
 }
 
 function reqsToMap(requisites) {
